a08f5786f8fb8de69784b15f2e599e0d4f8c4bab
genis <gbou@inscaparrella.cat>
2025-02-23 19:41:42 +0100

    Jsdoc class food

--- js/classes/food.js
+++ js/classes/food.js
@@ -1,13 +1,28 @@
-
+/**
+ * @type {gameObject}
+ */
 import { gameObject } from './gameObject.js';
 
+/**
+ * @class Food
+ * @extends gameObject
+ */
 export class Food extends gameObject {
+
+  /**
+   * Crea una nova instància de Food.
+   * @param {number} row - Numero de fila
+   * @param {number} column - Numero de columna
+   */
   constructor(row, column) {
     super(row, column);
     this.pointsFood = 10;
   }
 
   toString() {
+    /**
+     * Mostra la informació de l'objecte Food
+     */
     console.log( `Food at row ${this.rowNumber}
     and column ${this.columnObjectNumber}
     with ${this.pointsFood} points`);
